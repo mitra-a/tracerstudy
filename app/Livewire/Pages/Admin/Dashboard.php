96cd9c530e2f05fd8cf204ca2e2ad914d9b85d5d
mitra-a <mitra091202@gmail.com>
2024-04-01 08:40:00 +0800

    dashboard admin + pengunjung

--- app/Livewire/Pages/Admin/Dashboard.php
+++ app/Livewire/Pages/Admin/Dashboard.php
@@ -2,10 +2,37 @@
 
 namespace App\Livewire\Pages\Admin;
 
+use App\Models\Pengunjung;
+use App\Models\Periode;
+use App\Models\Prodi;
+use App\Models\User;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $arrayData = [];
+
+    public function mount(){
+        $this->arrayData['alumni'] = User::where('role','alumni')->count();
+        $this->arrayData['prodi'] = Prodi::count();
+        $this->arrayData['periode'] = Periode::count();
+        
+        $pengunjung = Pengunjung::selectRaw('sum(jumlah) as jumlah')->get();
+        $this->arrayData['pengunjung'] = $pengunjung[0]->jumlah;
+
+        $this->arrayData['login'] = User::whereNotNull('last_login_at')->orderBy('last_login_at', 'DESC')->limit(9)->get();
+
+        $week = Carbon::now();
+        $this->arrayData['pekan'] = [];
+        foreach([1,2,3,4,5,6,7,8,9,10] as $item){
+            $this->arrayData['pekan']['tanggal'][] = $week->format("d-M"); 
+            $pengunjung = Pengunjung::whereDate('created_at', $week)->first();
+            $this->arrayData['pekan']['data'][] = $pengunjung->jumlah ?? 0;
+            $week = $week->subDay()->copy();
+        }
+    }
+
     public function render()
     {
         return view('pages.admin.dashboard');
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,39 +1,121 @@
 <div>
+    @push('style')
+        <link rel="stylesheet" href="{{ asset('vendor/libs/apex-charts/apex-charts.css') }}" />
+    @endpush
+
+    @push('script')
+        <script src="{{ asset('vendor/libs/apex-charts/apexcharts.js') }}"></script>
+        <script>
+            let chartDOM = document.querySelector('#chart-data');
+            
+            function getOptions(){
+                return options = {
+                    series: [{
+                        name: "Jumlah Pengujung",
+                        data: JSON.parse(chartDOM.getAttribute('data-chart'))
+                    }],
+                    chart: {
+                        height: 350,
+                        type: 'area',
+                        zoom: {
+                            enabled: false
+                        }
+                    },
+                    labels:  JSON.parse(chartDOM.getAttribute('label-chart'))
+                };
+            }
+
+            var chart = new ApexCharts(chartDOM, getOptions());
+            chart.render();
+        </script>
+    @endpush
+
+    <div class="row">
+        <div class="col-lg-3 mb-4">
+            <div class="card">
+                <div class="card-body text-center">
+                    <div class="mb-3">
+                        <h1 class="text-primary font-weight-bold">{{ $arrayData['alumni'] }}</h1>
+                    </div>
+                    <h5 class="h3 font-weight-bolder mb-3">Alumni</h5><span class="d-block text-sm text-muted">Jumlah Alumni Terdaftar <br> dalam Database</span>
+                    </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+            <div class="card">
+                <div class="card-body text-center">
+                    <div class="mb-3">
+                        <h1 class="text-primary font-weight-bold">{{ $arrayData['prodi'] }}</h1>
+                    </div>
+                    <h5 class="h3 font-weight-bolder mb-3">Program Studi</h5><span class="d-block text-sm text-muted">Jumlah Program Studi <br> dalam Database</span>
+                    </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+            <div class="card">
+                <div class="card-body text-center">
+                    <div class="mb-3">
+                        <h1 class="text-primary font-weight-bold">{{ $arrayData['periode'] }}</h1>
+                    </div>
+                    <h5 class="h3 font-weight-bolder mb-3">Periode Wisuda</h5><span class="d-block text-sm text-muted">Jumlah Periode Wisuda <br> dalam Database</span>
+                    </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+            <div class="card">
+                <div class="card-body text-center">
+                    <div class="mb-3">
+                        <h1 class="text-primary font-weight-bold">{{ $arrayData['pengunjung'] }}</h1>
+                    </div>
+                    <h5 class="h3 font-weight-bolder mb-3">Pengunjung</h5><span class="d-block text-sm text-muted">Jumlah Pengunjung <br> Tracer Study</span>
+                    </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-lg-8 col-12">
-            <div class="row">
-                @foreach ([1,1,1] as $item)
-                    <div class="col-lg-4 mb-4">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <div class="mb-3">
-                                    <h1 class="text-primary font-weight-bold">195</h1>
-                                </div>
-                                <h5 class="h3 font-weight-bolder mb-1">Prodi</h5><span class="d-block text-sm text-muted">Jumlah Program Studi <br> dalam Database</span>
-                              </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
             <div class="card">
-                <div class="card-body">
+                <div class="card-header">
+                    <h5 class="card-title m-0 me-2">Pengunjung 10 hari terakhir</h5>
+                </div>
 
+                <div class="card-body">
+                    <div 
+                        data-chart="{{ json_encode($arrayData['pekan']['data']) }}"
+                        label-chart="{{ json_encode($arrayData['pekan']['tanggal']) }}"
+                        id="chart-data"></div>
                 </div>
             </div>
         </div>
 
         <div class="col-lg-4 col-12">
-            <h4 class="mb-4">Akun Terakhir Login</h4>
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title m-0 me-2">Login Terbaru</h5>
+                </div>
+                <div class="card-body">
+                    <ul class="p-0 m-0">
+                        @foreach ($arrayData['login'] as $item)
+                            <li class="d-flex @if(!$loop->last) mb-4 pb-1 @endif">
+                                <div class="avatar flex-shrink-0 me-3">
+                                    <img src="{{ asset('img/avatar.jpg') }}" alt class="w-px-40 rounded-circle" style="object-fit: cover" />
+                                </div>
 
-            @foreach ([1,1,1,1,1,1] as $item)
-            <div class="card mb-2">
-                <div class="card-body p-3">
-                    <h5 class="mb-0">NUR RESKY EVAWANTI</h5>
-                    <p class="mb-0 text-primary">mahasiswa</p>
-                    <p class="mb-0">2023-10-27 15:31:34</p>
+                                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                    <div class="me-2">
+                                        <h6 class="mb-1">{{ $item->nama }}</h6>
+                                        <small class="text-muted d-block mb-1">{{ $item->last_login_at->diffForHumans()g }}</small>
+                                    </div>
+                                    <div class="user-progress d-flex align-items-center gap-1">
+                                        <span class="badge bg-primary">{{ $item->role }}</span>
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
-            @endforeach
         </div>
     </div>
 </div>
