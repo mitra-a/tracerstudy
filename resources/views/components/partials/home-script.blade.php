0c8c0cb11c1ffcb9a782ac853da6dd065b869e96
mitra-a <mitra091202@gmail.com>
2024-04-25 15:51:09 +0800

    update asset home

--- resources/views/components/partials/home-script.blade.php
+++ resources/views/components/partials/home-script.blade.php
@@ -1,5 +1,5 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('home/jquery.min.js') }}"></script>
+<script src="{{ asset('home/bootstrap.min.js') }}"></script>
 <script src="{{ asset('home/lib/wow/wow.min.js') }}"></script>
 <script src="{{ asset('home/lib/easing/easing.min.js') }}"></script>
 <script src="{{ asset('home/lib/waypoints/waypoints.min.js') }}"></script>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -260,5 +260,5 @@
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
-            class="bi bi-arrow-up"></i></a>
+            class="fas fa-chevron-up"></i></a>
 </div>
