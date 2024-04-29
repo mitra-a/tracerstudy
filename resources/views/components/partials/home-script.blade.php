c0fe1e31a0c766a29c0f5665473f213920e7e576
mitra-a <mitra091202@gmail.com>
2024-04-29 14:15:58 +0800

    defer javascript

--- resources/views/components/partials/home-script.blade.php
+++ resources/views/components/partials/home-script.blade.php
@@ -1,8 +1,8 @@
-<script src="{{ asset('home/jquery.min.js') }}"></script>
-<script src="{{ asset('home/bootstrap.min.js') }}"></script>
-<script src="{{ asset('home/lib/wow/wow.min.js') }}"></script>
-<script src="{{ asset('home/lib/easing/easing.min.js') }}"></script>
-<script src="{{ asset('home/lib/waypoints/waypoints.min.js') }}"></script>
-<script src="{{ asset('home/lib/lightbox/js/lightbox.min.js') }}"></script>
+<script defer src="{{ asset('home/jquery.min.js') }}"></script>
+<script defer src="{{ asset('home/bootstrap.min.js') }}"></script>
+<script defer src="{{ asset('home/lib/wow/wow.min.js') }}"></script>
+<script defer src="{{ asset('home/lib/easing/easing.min.js') }}"></script>
+<script defer src="{{ asset('home/lib/waypoints/waypoints.min.js') }}"></script>
+<script defer src="{{ asset('home/lib/lightbox/js/lightbox.min.js') }}"></script>
 
-<script src="{{ asset('home/js/main.js') }}"></script>
+<script defer src="{{ asset('home/js/main.js') }}"></script>
